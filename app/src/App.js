e49bae22b888a9564b548334033ff3ca5f66a93a
markelo77 <pmarkelo77@gmail.com>
2021-11-23 16:15:49 +0400

    Add react-app

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+      <h3 className="d-flex justify-content-center m-3">
+        Учёт деятельности фитнес центров
+      </h3>
+    </div>
+  );
+}
+
+export default App;
