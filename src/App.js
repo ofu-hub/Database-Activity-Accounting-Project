21529e7f259f1512462274a3b8ce9456abc72e62
markelo77 <pmarkelo77@gmail.com>
2021-11-26 18:20:06 +0400

    Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Navbar,
+  Container,
+  Nav
+} from 'react-bootstrap';
+import { Routes } from 'react-router';
+import { Clients } from './Clients';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App container">
-      <h3 className="d-flex justify-content-center m-3">
-        Учёт деятельности фитнес центров
-      </h3>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/home" className="">Учёт деятельности фитнес центров</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/home">Главная</Nav.Link>
+                <Nav.Link href="/hall">Залы</Nav.Link>
+                <Nav.Link href="/trainers">Тренера</Nav.Link>
+                <Nav.Link href="/subs">Абонементы</Nav.Link>
+                <Nav.Link href="/visits">Посещения</Nav.Link>
+                <Nav.Link href="/payment">Выписки</Nav.Link>
+                <Nav.Link href="/clients">Клиенты</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        
+        <Routes>
+          <Route path="/clients" component={Clients}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
