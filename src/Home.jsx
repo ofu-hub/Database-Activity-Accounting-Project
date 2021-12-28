e67c48f65786dc8a5556e12e6dad34299aefd18c
markelo77 <pmarkelo77@gmail.com>
2021-12-28 05:41:56 +0400

    Ready frontend

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,61 @@
-import React from "react";
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { MDBDataTable } from 'mdbreact';
 
 function Home() {
+
+    const [visits, setVisits] = useState([]);
+
+    const userAttributes = []
+    visits.map(visit=>{
+        userAttributes.push({
+            id: visit.ID_Subscription,
+            date: visit.DateTime
+        })
+    });
+
+    function DatatablePage() {
+        const data = {
+          columns: [
+            {
+              label: 'Номер абонемента',
+              field: 'id',
+              sort: 'asc',
+              width: 150
+            },
+            {
+              label: 'Дата',
+              field: 'date',
+              sort: 'asc',
+              width: 270
+            }
+          ],
+          rows: userAttributes
+        };
+
+        return (
+            <MDBDataTable
+              entriesLabel={'Показать записи'}
+              infoLabel={["Показано с", "по", "из", "записей"]}
+              paginationLabel={['Предыдущая', 'Следующая']}
+              searchLabel={'Поиск'}
+              noRecordsFoundLabel={'Ничего не найдено'}
+              scrollY
+              maxHeight="200px"
+              striped
+              bordered
+              small
+              data={data}
+            />
+          );
+    }
+
+    useEffect(() =>{
+        axios.get('/api/Visit/NowDate')
+            .then(res => setVisits(res.data))
+            .catch(err => console.log(err));
+    }, [])
+
     return (
         <div>
             <div className="m-2">
@@ -8,15 +63,22 @@ function Home() {
                     <h3>Главная страница</h3>
                 </div>
             </div>
-            <div className="d-flex justify-content-between m-2 p-3">
-                <div className="d-flex align-items-center">
+            <div className="d-flex justify-content-sm-between m-2 p-3">
+                <div className="flex-row align-items-center">
                     <h5>Таблица посещений на сегодня</h5>
+                    {DatatablePage()}
                 </div>
-                <div className="d-flex align-items-center">
+                <div className="pl-5 pr-5 flex-row align-items-center">
+                    <div>
                     <h5>Немного об учете</h5>
+                        <div>
+                        <h8 className="center-block">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis provident adipisci minus facilis natus molestiae harum commodi, deserunt optio dicta veritatis error vel aliquam dolorem beatae nemo ad perferendis eligendi?
+                    Fugit consectetur placeat perspiciatis fuga aliquam sed facilis similique itaque, quia ab asperiores debitis vel harum nostrum eum nihil alias recusandae? Nemo dolore commodi corrupti dolorum cum necessitatibus voluptates enim.
+                    Nesciunt in blanditiis repellat praesentium eaque hic tenetur reiciendis nisi facere nostrum possimus sunt, molestias debitis, odio consequatur. Quasi doloremque dicta illo numquam maxime quisquam totam nesciunt obcaecati voluptas quidem.</h8>
+                        </div>
+                    </div>
                 </div>
-                <div className="d-flex align-items-center">
-                    <h5>О программе</h5>
+                <div className="flex-row align-items-center">
                 </div>
             </div>
         </div>
--- src/Payment.jsx
+++ src/Payment.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
 
 function Payment() {
+    const [payments, setPayments] = useState([]);
+
+    useEffect(() =>{
+        axios.get('/api/Payment')
+            .then(res => setPayments(res.data))
+            .catch(err => console.log(err));
+    }, [])
+
     return (
         <div>
             <div className="m-2">
-                <div className="pl-5 pr-5 pt-4">
+                <div className="row pl-5 pr-5 pt-4">
                     <h3>Выписки</h3>
                 </div>
+            </div>
+            <div>
+                <table className="table table-bordered text-center">
+                    <thead>
+                        <tr>
+                            <th>
+                                Номер абонемента
+                            </th>
+                            <th>
+                                Дата
+                            </th>
+                            <th>
+                                Цена
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {payments.map(pay =>
+                            <tr key={pay.ID_Subscription}>
+                                <td>{pay.ID_Subscription}</td>
+                                <td>{pay.Date}</td>
+                                <td>{pay.Price}</td>
+                            </tr>    
+                        )}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
